58300428b98f98b83e87f45d574d68062fa6f15b
lefarmer01600 <59290007+lefarmer01600@users.noreply.github.com>
2022-11-09 14:14:07 +0100

    Update form.js

--- public/js/form.js
+++ public/js/form.js
@@ -4,7 +4,8 @@ $(function() {
       get: (searchParams, prop) => searchParams.get(prop),
     });
     $('.p-t-136').text(params.firstname + " " + params.name)
-    var host = location.origin.replace(/^http/, 'ws')
+    let host = location.origin.replace(/^http/, 'ws')
+    console.log(host)
     const ws = new WebSocket(host)
     //const ws = new WebSocket("ws://localhost:8000")
 
