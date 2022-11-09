e355dbdc082e0c5f1ebca4223bb9be4efd0952bd
lefarmer01600 <59290007+lefarmer01600@users.noreply.github.com>
2022-11-09 14:06:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,154 @@
-// const express = require('express')
 import express from 'express';
 const app = express()
 import WebSocket, { WebSocketServer } from "ws";
-app.use(express.static('public'));
+
+import bodyParser from "body-parser"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-//const wss = new WebSocketServer({ port: 3000 });
-
-let response
 
 let CurrentQuestion = 0
 
-let questions = [{ question: "Première question : <br> Etes vous égoiste ou altruiste", response: ["égoiste", "altruiste"] }, {}, {}]
+
+let questions = [
+  {
+    question: "Etes-vous égoïste ou altruiste",
+    response: ["égoiste", "altruiste"]
+  },
+  {
+    question: "Si quelqu'un se fait agresser par plusieurs personnes armées dans la rue",
+    response: ["Vous partez", "Vous aidez la personne"]
+  },
+  {
+    question: "Si un membre de votre famille se fait agresser par plusieurs personnes armées dans la rue",
+    response: ["Vous partez", "Vous aidez la personne"]
+  },
+  {
+    question: "Pensez vous qu'avec l'age on devient :",
+    response: ["aucun des deux", "Altruiste", "égoïste"]
+  },
+  {
+    question: "Selon vous, existe-t'il différent type d'altruisme et d'égoïsme",
+    response: ["non", "oui"]
+  },
+  {
+    question: "Pour vous, être égoïste/altruiste, c'est :",
+    response: ["Un choix", "Un instinct"]
+  },
+  {
+    question: "Etes-vous égoïste ou altruiste",
+    response: ["égoiste", "altruiste"]
+  }
+]
 
 let questionRespones = []
+let UsersPerQuestion = []
+let Users = []
+
 
 for (let i = 0; i < questions.length; i++) {
-    questionRespones.push([])
+  questionRespones.push({})
+  UsersPerQuestion.push([])
 }
 
-app.get('/form', function (req, res) {
-    response = {
-        firstname: req.query.firstname,
-        name: req.query.name
-    };
-    res.sendFile(__dirname + "/public/form.html");
+app.use(express.static('public'))
+app.get('/form', function(req, res) {
+  let response = {
+    firstname: req.query.firstname,
+    name: req.query.name
+  };
+  if (!(Users.some(e => e.firstname === response.firstname) && Users.some(e => e.name === response.name))) {
+    Users.push({ firstname: response.firstname, name: response.name })
+  }
+  res.sendFile(__dirname + "/public/form.html");
+})
+app.get('/admin', function(req, res) {
+  let response = {
+    firstname: req.query.firstname,
+    name: req.query.name
+  };
+  if (response.name == "clemenceau" && response.firstname == "thomas") {
+    res.sendFile(__dirname + "/public/admin.html");
+  } else {
+    res.send("Cannot GET /test")
+  }
 })
 
-//var server = app.listen(443, "0.0.0.0", function () {
-var server = app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
 
-    var host = server.address().address
-    var port = server.address().port
+app.get('/users', function(req, res) {
+  let response = {
+    firstname: req.query.firstname,
+    name: req.query.name
+  };
+  res.sendFile(__dirname + "/public/users.html");
+})
 
-    console.log('Express app listening at http://%s:%s', host, port)
+
+let jsonParser = bodyParser.json()
+app.post('/admin', jsonParser, function(req, res) {
+  if (req.body.type === "nextquestion" && questions.length > CurrentQuestion + 1) {
+    CurrentQuestion += 1
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "SendData", showquestion: true, data: questions[CurrentQuestion] }));
+      }
+    });
+    res.send(JSON.stringify({ CurrentQuestionNumber: CurrentQuestion, CurrentQuestion: questions[CurrentQuestion] }));
+  } else if (req.body.type === "resetquestion") {
+    questionRespones = []
+    UsersPerQuestion = []
+
+    for (let i = 0; i < questions.length; i++) {
+      questionRespones.push({})
+      UsersPerQuestion.push([])
+    }
+    CurrentQuestion = 0
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "SendData", showquestion: true, data: questions[CurrentQuestion] }));
+      }
+    });
+    res.send(JSON.stringify({ CurrentQuestionNumber: CurrentQuestion, CurrentQuestion: questions[CurrentQuestion] }));
+  }
+});
+
+var server = app.listen(process.env.PORT || 3000, "0.0.0.0", function() {
+  //var server = app.listen(8000, "0.0.0.0", function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Express app listening at http://%s:%s', host, port)
 
 })
 
 const wss = new WebSocketServer({ server: server })
 
 wss.on("connection", function connection(ws) {
-    ws.on("message", function message(message) {
-        const data = JSON.parse(message);
-        switch (data.type) {
-            // case "DataMessage":
-            //     wss.clients.forEach((client) => {
-            //         if (client !== ws && client.readyState === WebSocket.OPEN) {
-            //             InfoSave["PID" + data.data.id] = data.data
-            //             client.send(JSON.stringify({ type: "DataMessage", data: data.data }));
-            //         }
-            //     });
-            //     break;
-            case "RequestData":
-                wss.clients.forEach((client) => {
-                    if (client == ws && client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: "SendData", data: questions[CurrentQuestion] }));
-                    }
-                });
-                break;
-            case "Response":
-                console.log(data)
-                questionRespones[CurrentQuestion].push({ "name": data.name, "firstname": data.firstname, "value": data.value })
-                console.log(data, JSON.stringify(questionRespones[CurrentQuestion]))
-                break;
-        }
-    });
+  ws.on("message", function message(message) {
+    const data = JSON.parse(message);
+    switch (data.type) {
+      case "RequestData":
+        wss.clients.forEach((client) => {
+          if (client == ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "SendData", showquestion: !UsersPerQuestion[CurrentQuestion].includes(data.firstname + " " + data.name), data: questions[CurrentQuestion] }));
+          }
+        });
+        break;
+      case "RequestAllData":
+        wss.clients.forEach((client) => {
+          if (client == ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "SendAllData", data: questionRespones, questions: questions, users: Users }));
+          }
+        });
+        break;
+      case "Response":
+        questionRespones[CurrentQuestion][data.firstname + "." + data.name] = { "name": data.name, "firstname": data.firstname, "value": data.value }
+        UsersPerQuestion[CurrentQuestion].push(data.firstname + " " + data.name)
+        break;
+    }
+  });
 });
